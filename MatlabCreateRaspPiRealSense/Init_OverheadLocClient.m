7a1d90a815919ea8c476606fc1552d6bba92b82b
Liran Gazit <47790066+lirangazit@users.noreply.github.com>
2021-02-05 09:57:27 -0500

    Modified to remove hardcoded IP of Optitrack PC, and instead look for the DNS registration

--- MatlabCreateRaspPiRealSense/Init_OverheadLocClient.m
+++ MatlabCreateRaspPiRealSense/Init_OverheadLocClient.m
@@ -2,7 +2,9 @@ function OverheadLocClient = Init_OverheadLocClient()
     % This function initializes a NatNet Client to read streaming object tracking data
     % from the Optitrack Motive server
     % The NatNet SDK from Optitrack has to be in the Matlab Path
-
+	% Liran 2020, 2021
+	
+	
     % find local IP used for natnet client
     [~, result] = system('ipconfig');
     start = strfind(result,'IPv4');
@@ -16,10 +18,16 @@ function OverheadLocClient = Init_OverheadLocClient()
     OverheadLocClient = natnet;
 
     % connect the client to the server (multicast over LAN) -
-    OverheadLocClient.HostIP = '128.253.194.102';
+    % If you have the IP for the Optitrack PC:
+    % OverheadLocClient.HostIP = '128.253.115.218';
+    
+    % If you have the DNS hostname for the Optitrack PC:
+    OverheadLocClient.HostIP = resolvehost('optitrack-pc.labs.mae.cornell.edu','address'); 
+    
     OverheadLocClient.ClientIP = myIP;
     OverheadLocClient.ConnectionType = 'Multicast';
     OverheadLocClient.connect;
+    
     
     if ( OverheadLocClient.IsConnected == 0 )
         fprintf( '\tNatEnt Client failed to connect\n' )
