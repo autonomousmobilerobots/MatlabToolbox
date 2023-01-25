e66c34843ae745e1bdfc4db7ddb032dcde28759c
Liran Gazit <47790066+lirangazit@users.noreply.github.com>
2023-01-25 10:49:50 -0500

    Add files via upload
    Return NaN if encounters an error

--- MatlabCreateRaspPiRealSense/OverheadLocalizationCreate.m
+++ MatlabCreateRaspPiRealSense/OverheadLocalizationCreate.m
@@ -1,14 +1,24 @@
-function [poseX, poseY, poseTheta] = OverheadLocalizationCreate(Robot)
-% This function returns the latest Optitrack pose of 'Robot'
-% Wrapper for 'Create_Optitrack_Pose', so students can use the same function as the simulator.
+function[poseX, poseY, poseTheta] = OverheadLocalizationCreate(Robot)
+% Dummy wrapper so we can use the same function in the lab and the simulator.
+% 
+% Robot is the robot struct created by CretePiInit
 %
-% Robot struct is created by CreatePiInit function
-%
-% poseX, poseY in meters
-% poseTheta in radians
+% Output: PoseX, poseY, poseTheta
+% In case of an error, returns NaNs
+  
 
-Pose = Create_Optitrack_Pose(Robot.Name, Robot.OL_Client);
-poseX = Pose(1);
-poseY = Pose(2);
-poseTheta = Pose(3);
+    poseX = NaN;
+    poseY = NaN;
+    poseTheta = NaN;
 
+	try
+		Pose = Create_Optitrack_Pose(Robot.Name, Robot.OL_Client);
+		poseX = Pose(1);
+		poseY = Pose(2);
+		poseTheta = Pose(3);
+
+	catch
+		disp(append('WARNING: function ', mfilename, ' did not execute correctly'));    
+	end
+
+end
