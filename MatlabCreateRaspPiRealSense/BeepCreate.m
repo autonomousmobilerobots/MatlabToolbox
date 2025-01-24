ff419fd72c9f90a47814546d578d10509f0ce948
Liran Gazit <47790066+lirangazit@users.noreply.github.com>
2025-01-24 10:03:02 -0500

    Add files via upload

--- MatlabCreateRaspPiRealSense/BeepCreate.m
+++ MatlabCreateRaspPiRealSense/BeepCreate.m
@@ -1,17 +1,13 @@
 function[] = BeepCreate(Robot)
 % Wrapper for BeepRoomba.
-% 
+%
 % Robot is the robot struct created by CretePiInit
 %
 %
 %% Liran 2023
-  
 
-
-	try
-		BeepRoomba(Robot.CreatePort)
-	catch
-		disp(append('WARNING: function ', mfilename, ' did not execute correctly'));    
-	end
-
+try
+    BeepRoomba(Robot.CreatePort)
+catch
+    disp(append('WARNING: function ', mfilename, ' did not execute correctly'));
 end
